ecf61db61316c1c7135fe1c358641764ba4cafa4
박창현 <buschanny@gmail.com>
2022-04-15 09:32:21 +0900

    modified Prog 모의고사

--- 220412/prog_mockExam.java
+++ 220412/prog_mockExam.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 public class prog_mockExam {
     class Solution {
         public int[] solution(int[] answers) {
+            ArrayList<Integer> list = new ArrayList<Integer>();
+
             int[] first = {1,2,3,4,5};
             int[] second = {2,1,2,3,2,4,2,5};
             int[] third = {3,3,1,1,2,2,4,4,5,5};
@@ -14,18 +16,18 @@ public class prog_mockExam {
                 if(second[i % 8] == answers[i]) scores[1]++;
                 if(third[i % 10] == answers[i]) scores[2]++;
             }
-            ArrayList<Integer> list = new ArrayList<Integer>();
 
             int[] arr = new int[scores.length];
             for(int i = 0; i < scores.length; i++){
                 arr[i] = scores[i];
             }
 
+            // 최댓값
             Arrays.sort(arr);
 
             for(int i = 0; i < arr.length; i++){
                 if(scores[i] == arr[2]){
-                    list.add(i+1);
+                    list.add(i+1);  // 인덱스 1부터
                 }
             }
 
