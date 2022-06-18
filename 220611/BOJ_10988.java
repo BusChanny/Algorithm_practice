a782c621080e163cbad0aa6ca334f00f41131b65
ChangHyun Park <buschanny@gmail.com>
2022-06-18 23:33:21 +0900

    solved: BOJ 10988 펠린드롬인지 확인하기

---
+++ 220611/BOJ_10988.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10988 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int ans = 1;
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt((str.length() - 1) - i)) {
+                ans = 0;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
